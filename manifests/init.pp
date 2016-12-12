e517cf2cf8075b4a44631e8e33fff93e6609966b
Nick Miller <nick.miller@onyxpoint.com>
2016-12-12 11:42:14 -0500

    Added dns autoconf back

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,19 +18,35 @@
 # @param resolv_domain
 #   Local domain name, defaults to the domain of your host.
 #
+# @param named_server
+#   A boolean that states that this server is definitively a named server.
+#   Bypasses the need for $named_autoconf below.
+# @param named_autoconf
+#   A boolean that controlls whether or not to autoconfigure named.
+#   true => If the server where puppet is being run is in the list of
+#           $servers then automatically configure named.
+#   false => Do not autoconfigure named.
+# @param caching
+#   *If* the $servers array above starts with '127.0.0.1' or '::1', then
+#   the system will set itself up as a caching nameserver unless this is set
+#   to false.
+#
 class resolv (
-  Array           $servers               = simplib::lookup('::simp_options::dns::servers', { 'default_value' => ['127.0.0.1'], 'value_type'      => Array[String] }),
-  Array           $search                = simplib::lookup('::simp_options::dns::search', { 'default_value'  => [$facts['domain']], 'value_type' => Array[String] }),
-  Optional[Array] $sortlist              = undef,
-  Optional[Array] $extra_options         = undef,
-  String          $resolv_domain         = $facts['domain'],
-  Boolean         $debug                 = false,
-  Boolean         $rotate                = true,
-  Boolean         $no_check_names        = false,
-  Boolean         $inet6                 = false,
-  Integer         $ndots                 = 1,
-  Integer         $timeout               = 2,
-  Integer         $attempts              = 2
+  Array           $servers        = simplib::lookup('::simp_options::dns::servers', { 'default_value' => ['127.0.0.1'], 'value_type'      => Array[String] }),
+  Array           $search         = simplib::lookup('::simp_options::dns::search', { 'default_value'  => [$facts['domain']], 'value_type' => Array[String] }),
+  Optional[Array] $sortlist       = undef,
+  Optional[Array] $extra_options  = undef,
+  String          $resolv_domain  = $facts['domain'],
+  Boolean         $debug          = false,
+  Boolean         $rotate         = true,
+  Boolean         $no_check_names = false,
+  Boolean         $inet6          = false,
+  Integer         $ndots          = 1,
+  Integer         $timeout        = 2,
+  Integer         $attempts       = 2,
+  Boolean         $named_server   = false,
+  Boolean         $named_autoconf = true,
+  Boolean         $caching        = true
 ) {
   if $sortlist { validate_net_list($sortlist) }
 
@@ -40,4 +56,49 @@ class resolv (
     mode    => '0644',
     content => template('resolv/etc/resolv.conf.erb')
   }
+
+  # If this client is one of these passed IP's, then make it a real DNS server
+  if $named_server or (defined('named') and defined(Class['named'])) or ($named_autoconf and host_is_me($servers)) {
+    $l_is_named_server = true
+  }
+  else {
+    $l_is_named_server = false
+  }
+
+  if $named_autoconf {
+    # Having 127.0.0.1 or ::1 first tells us that we want to be a
+    # caching DNS server.
+    if ! $l_is_named_server and $caching and ($servers[0] == '127.0.0.1' or $servers[0] == '::1' ) {
+      if size($servers) == 1 {
+        fail('If using named as a caching server, 127.0.0.1 must not be your only nameserver entry.')
+      }
+      else {
+        include '::named::caching'
+
+        $l_forwarders = inline_template('<%= @servers[1..-1].join(" ") %>')
+        named::caching::forwarders { $l_forwarders: }
+      }
+    }
+    else {
+      if $l_is_named_server {
+        include '::named'
+      }
+    }
+  }
+
+  # We're managing resolv.conf, so ignore what dhcp says.
+  simp_file_line { 'resolv_peerdns':
+    path       => '/etc/sysconfig/network',
+    line       => 'PEERDNS=no',
+    match      => '^\s*PEERDNS=',
+    deconflict => true
+  }
+
+  if defined_with_params(Class['named'], {'chroot' => false}) {
+      $bind_pkg = 'bind'
+  }
+  else {
+    $bind_pkg = 'bind-chroot'
+  }
+
 }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -5,6 +5,11 @@ describe 'resolv' do
     on_supported_os.each do |os, facts|
       context "on #{os}" do
         let(:facts) do
+          facts[:fqdn] = 'foo.bar.baz'
+          facts[:hostname] = 'foo'
+          facts[:interfaces] = 'eth0'
+          facts[:ipaddress_eth0] = '10.0.2.15'
+
           facts
         end
 
@@ -13,12 +18,19 @@ describe 'resolv' do
         }}
 
         context 'with default parameters' do
+          let(:params) {{
+            :servers => ['1.2.3.4','5.6.7.8']
+            }}
           let(:expected) { File.read('spec/expected/default_resolv.conf') }
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to contain_file('/etc/resolv.conf').with_content(expected) }
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to contain_simp_file_line('resolv_peerdns') }
+          it { is_expected.not_to contain_class('named') }
+          it { is_expected.not_to contain_class('named::caching') }
         end
 
-        context 'with everything set' do
+        context 'resolv.conf with everything set' do
           let(:params) {{
             :servers => ['1.2.3.4','5.6.7.8'],
             :search  => ['test.net'],
@@ -36,6 +48,63 @@ describe 'resolv' do
           let(:expected) { File.read('spec/expected/fancy_resolv.conf') }
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to contain_file('/etc/resolv.conf').with_content(expected) }
+        end
+
+        context 'node_is_nameserver' do
+          let(:facts) { facts.merge({:ipaddress => '10.0.2.15'}) }
+
+          let(:params) {{
+            :servers => ['1.2.3.4','5.6.7.8','10.0.2.15']
+          }}
+
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.not_to contain_class('named::caching') }
+          # it { is_expected.to contain_class('named') }
+        end
+
+        context 'node_is_nameserver_with_selinux' do
+          let(:facts) { facts.merge({
+            :fqdn => 'foo.bar.baz',
+            :hostname => 'foo',
+            :interfaces => 'eth0',
+            :ipaddress_eth0 => '10.0.2.15',
+            :selinux_enforced => true,
+          }) }
+          let(:params) {{
+            :servers => ['1.2.3.4','5.6.7.8','10.0.2.15']
+          }}
+
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.not_to contain_class('named::caching') }
+          it { is_expected.to contain_class('named') }
+        end
+
+        context 'node_with_named_autoconf_and_caching' do
+          let(:facts) { facts.merge({
+            :fqdn => 'foo.bar.baz',
+            :hostname => 'foo',
+            :interfaces => 'eth0',
+            :ipaddress_eth0 => '10.0.2.15',
+          }) }
+          let(:params) {{
+            :servers => ['127.0.0.1','1.2.3.4','5.6.7.8']
+          }}
+
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to contain_class('named::caching') }
+        end
+
+        context 'node_with_named_autoconf_and_caching_only_127.0.0.1' do
+          let(:facts) { facts.merge({
+            :fqdn => 'foo.bar.baz',
+            :hostname => 'foo',
+            :interfaces => 'eth0',
+            :ipaddress_eth0 => '10.0.2.15',
+          }) }
+          let(:params) {{
+            :servers => ['127.0.0.1']
+          }}
+          it { expect { is_expected.to compile.with_all_deps}.to raise_error(/not be your only/) }
         end
 
       end
