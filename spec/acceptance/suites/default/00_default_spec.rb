c29e5a5cce9f1fee40680c46f246d52ce2f0c4da
Steven Pritchard <steven.pritchard@onyxpoint.com>
2020-06-17 11:09:36 -0500

    Clean up acceptance test.

--- spec/acceptance/suites/default/00_default_spec.rb
+++ spec/acceptance/suites/default/00_default_spec.rb
@@ -6,8 +6,8 @@ describe 'resolv' do
 
   servers = ['8.8.8.8', '8.8.4.4', '1.1.1.1']
 
-  hosts.each do |host|
-    let(:manifest) { <<~EOF
+  let(:manifest) do
+    <<~EOF
       class { 'resolv':
         servers                   => #{servers},
         use_nmcli                 => true,
@@ -15,32 +15,31 @@ describe 'resolv' do
         nmcli_ignore_auto_dns     => true,
         nmcli_auto_reapply_device => true,
       }
-      EOF
-    }
+    EOF
+  end
 
-    hosts.each do |host|
-      context "on #{host}" do
+  hosts.each do |host|
+    context "on #{host}" do
 
-        it 'should apply with no errors' do
-          apply_manifest_on(host, manifest)
-        end
+      it 'should apply with no errors' do
+        apply_manifest_on(host, manifest)
+      end
 
-        it 'should be idempotent' do
-          apply_manifest_on(host, manifest, catch_changes: true)
-        end
+      it 'should be idempotent' do
+        apply_manifest_on(host, manifest, catch_changes: true)
+      end
 
-        it 'should list all of the new DNS servers in the device information' do
-          device = pfact_on(host, 'defaultgatewayiface')
+      it 'should list all of the new DNS servers in the device information' do
+        device = pfact_on(host, 'defaultgatewayiface')
 
-          nameservers = on(
-            hosts.find { |h| h != host },
-            %{nmcli -f ip4.dns device show #{device} | awk '{print $2}' | sort | tr '\n' ' '},
-            :accept_all_exit_codes => true
-          ).stdout.split(' ')
+        result = on host,
+          %{nmcli -f ip4.dns device show #{device} | awk '{print $2}'},
+          :accept_all_exit_codes => true
 
-          # Ensure list of nameservers matches the nameservers declared in the Puppet manifest
-          expect(nameservers).to eq(servers)
-        end
+        nameservers = result.stdout.split("\n")
+
+        # Ensure list of nameservers matches the nameservers declared in the Puppet manifest
+        expect(nameservers).to eq(servers)
       end
     end
   end
